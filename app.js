fb944b8717926bdc616fe498f72e14d8b0ed6239
arthur andrade <arthurandrad.23@gmail.com>
2025-02-07 20:14:55 -0300

    Feat: Add a root route to return a greeting message from the Node.js server

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/workouts', workoutRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js server on Vercel!");
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
